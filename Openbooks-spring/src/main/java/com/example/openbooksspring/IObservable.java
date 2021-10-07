58a37b46d2523f460b44f0bbc44e3ef4b6a59ffc
Maksat <boktaban@gmail.com>
2021-10-07 23:39:17 +0600

    Extra files delition

--- Openbooks-spring/src/main/java/com/example/openbooksspring/IObservable.java
+++
@@ -1,7 +0,0 @@
-//package com.example.openbooksspring;
-//
-//public interface IObservable {
-//    boolean addBook(Book book);
-//    boolean removeBook(Book book);
-//    void notifyObservers();
-//}
--- Openbooks-spring/src/main/java/com/example/openbooksspring/IObserver.java
+++
@@ -1,7 +0,0 @@
-//package com.example.openbooksspring;
-//
-//import java.util.List;
-//
-//public interface IObserver {
-//    String update(List<Book> books);
-//}
